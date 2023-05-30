1b006daf9141193bdbd97c5b436005c5a8e18687
daimaruhk <daimaruhk@duck.com>
2023-05-30 16:50:18 -0400

    Remove "Storage" permission

--- scripts/background.ts
+++ scripts/background.ts
@@ -1,8 +1,11 @@
-chrome.storage.session.setAccessLevel({
-  accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS'
-});
+import { MessageType } from './utils';
 
 chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
-  // store the request header in the session storage, and use the header to make backup request.
-  chrome.storage.session.set({ request: details });
+  // send the request header to the content script, save it in window.sessionStorage, and use the it to make backup request.
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }).then((tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id!, {
+      type: MessageType.RequestHeader,
+      payload: details
+    });
+  });
 }, { urls: ['https://lihkg.com/api_v2/thread/*/page/*'] }, ['requestHeaders']);
