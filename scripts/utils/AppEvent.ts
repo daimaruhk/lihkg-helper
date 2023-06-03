6708b2de54feab177f38e2dc40df0a0915e396ae
daimaruhk <daimaruhk@duck.com>
2023-06-02 23:31:02 -0400

    feat: display a confirmation dialog when user attempts to delete a post

--- scripts/utils/AppEvent.ts
+++ scripts/utils/AppEvent.ts
@@ -1,7 +1,10 @@
 export default class AppEvent {
-  public static readonly CreateDesktopNavButton = 'app:create-desktop-nav-btn';
-  public static readonly CreateMobileNavButton = 'app:create-mobile-nav-btn';
+  public static readonly CreateNavButton = 'app:create-nav-btn';
   public static readonly CreateDrawerButton = 'app:create-drawer-btn';
-  public static readonly OnBackupStart = 'app:backup-start';
-  public static readonly OnBackupComplete = 'app:backup-complete';
+  public static readonly CreateDeleteButton = 'app:create-delete-btn';
+  public static readonly OnBackupStart = 'app:on-backup-start';
+  public static readonly OnBackupComplete = 'app:on-backup-complete';
+  public static readonly OnModalOpen = 'app:on-modal-open';
+  public static readonly OnModalClose = 'app:on-modal-close';
+  public static readonly DeleteMode = 'app:delete-mode';
 };
